b1d0b9c6a9bf33cec094c19a5120f56419b80151
steve <steven.fullhouse@gmail.com>
2020-05-08 13:46:53 +0100

    Comments added.

--- src/main/kotlin/DgnV8Detector.kt
+++ src/main/kotlin/DgnV8Detector.kt
@@ -16,20 +16,24 @@ import java.io.InputStream
  *    b) it contains a set of files with names that SEEM to be common to all DGN v8 files.
  */
 class DgnV8Detector : Detector {
-    // Filenames found in a DGN v8 compound file that will convince us that it is a DGN v8.
-    private val dgnV8SignatureFiles = setOf("Dgn~Mf", "Dgn~S", "Dgn~H", "\u0005SummaryInformation")
-
     override fun detect(input: InputStream, metadata: Metadata): MediaType {
         // Check for DGN8, Microsoft Compound Document, containing telltale files.
         // Apache POI is the boy for that job.
+
+        // We are obliged to leave the input stream at the same position as we got it.
         val tikaInputStream = input as TikaInputStream
         val inputLength = tikaInputStream.length.toInt()
         input.mark(inputLength)
         try {
+            // Unfortunately, POI has other ideas. So we need access to the whole file (for
+            // POI purposes) but we don't want to allow POI to do any annoying mark/reset/close
+            // operations that will interfere with Tika. Only way I can see around this is to
+            // read the whole file into memory or transfer it to a temporary file. For now, I'm
+            // going down the memory route.
             val dgnBytes = input.readNBytes(inputLength)
             val poiDoc = POIFSFileSystem(ByteArrayInputStream(dgnBytes))
             // If we find all the "signature" filenames, then we're happy.
-            if (poiDoc.root.filterIsInstance<DocumentEntry>().map { it.name }.containsAll(dgnV8SignatureFiles))
+            if (poiDoc.root.filterIsInstance<DocumentEntry>().map { it.name }.containsAll(DGN_V8_SIGNATURE_FILES))
                 return DGN_V8_MEDIA_TYPE
         } catch (e: Exception) {
             // Thrown if:
@@ -44,7 +48,10 @@ class DgnV8Detector : Detector {
     }
 }
 
+// Filenames found in a DGN v8 compound file that will convince us that it is a DGN v8.
+private val DGN_V8_SIGNATURE_FILES = setOf("Dgn~Mf", "Dgn~S", "Dgn~H", "\u0005SummaryInformation")
+
 // Not easy to find, but the official MIME types of DGN files are mentioned on this Bentley page:
 // https://communities.bentley.com/products/projectwise/content_management/w/wiki/5617/5617
 val DGN_V7_MEDIA_TYPE = MediaType("image", "vnd.dgn")
-val DGN_V8_MEDIA_TYPE = MediaType("image", "vnd.dgn", mapOf(Pair("ver", "8")))
+val DGN_V8_MEDIA_TYPE = MediaType(DGN_V7_MEDIA_TYPE, mapOf(Pair("ver", "8")))
